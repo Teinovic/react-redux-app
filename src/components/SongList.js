2b5d94bc38120f3ffdc8a8c1a5f71fa6f1651fc9
Teinovic <momir.teinovic.2011@gmail.com>
2021-04-14 20:59:31 +0200

    solved some problems that were causing the songlist not to have a working selectSong function

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,31 +1,35 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { selectSong } from '../actions'
+import React from 'react';
+import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
-function renderList({songs}) {
-    return songs.map(song => {
-        return (
-            <div className="item" key={song.title}>
-                <div className="right floated content">
-                    <button className="ui button primary"
-                            onClick={() => selectSong(song)}
-                    >
-                        Select
-                    </button>
-                </div>
-                <div className="content">{song.title}</div>
-            </div>
-        )
-    })
+function renderList(props) {
+    return props.songs.map(song => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button
+              className="ui button primary"
+              onClick={() => props.selectSong(song)}
+            >
+              Select
+            </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
+
+function SongList(props) {
+    return <div className="ui divided list">{renderList(props)}</div>;
 }
 
-function SongList({songs}) {
-    return <div className="ui divided list">{renderList({songs})}</div>
-}
+const mapStateToProps = state => {
+  console.log(state);
+  return { songs: state.songs };
+};
 
-const mapStateToProps = (state) => {    
-    console.log(state)
-    return { songs: state.songs }
-}
-
-export default connect(mapStateToProps, {selectSong})(SongList)  
+export default connect(
+  mapStateToProps,
+  { selectSong }
+)(SongList);
