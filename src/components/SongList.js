64f6e554f299c264b772c60287e47e508257b562
Teinovic <momir.teinovic.2011@gmail.com>
2021-04-14 19:17:57 +0200

    refactored songs class to a function

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { selectSong } from '../actions'
 
-class SongList extends Component {
-    renderList() {
-        return this.props.songs.map(song => {
-            return (
-                <div className="item" key={song.title}>
-                    <div className="right floated content">
-                        <button className="ui button primary"
-                                onClick={() => this.props.selectSong(song)}
-                        >
-                            Select
-                        </button>
-                    </div>
-                    <div className="content">{song.title}</div>
+function renderList({songs}) {
+    return songs.map(song => {
+        return (
+            <div className="item" key={song.title}>
+                <div className="right floated content">
+                    <button className="ui button primary"
+                            onClick={() => selectSong(song)}
+                    >
+                        Select
+                    </button>
                 </div>
-            )
-        })
-    }
-    
-    render() {
-        return <div className="ui divided list">{this.renderList()}</div>
-    }
+                <div className="content">{song.title}</div>
+            </div>
+        )
+    })
+}
+
+function SongList({songs}) {
+    return <div className="ui divided list">{renderList({songs})}</div>
 }
 
 const mapStateToProps = (state) => {    
